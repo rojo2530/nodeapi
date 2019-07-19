33ba827f017e7fc9077282febbc2452df4fcc846
rojo2530 <jcantosg@gmail.com>
2019-07-19 09:59:53 +0200

    Cambio ruta users por apiv1

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/',      require('./routes/index'));
-app.use('/users', require('./routes/users'));
+app.use('/apiv1', require('./routes/apiv1'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
